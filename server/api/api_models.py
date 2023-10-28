155a4fe530047f87c8a4b246ae9bba091607456a
kipkirui63 <enocksang8356@gmail.com>
2023-10-28 10:19:33 +0300

    added api models

--- server/api/api_models.py
+++ server/api/api_models.py
@@ -112,7 +112,64 @@ cart_items_schema = api.model('cart_items', {
 })
 
 
+carts_output_schema = api.model('carts_output',{
+    "id": fields.Integer,
+    "user_id":fields.Integer,
+    "created_at": fields.DateTime,
+    "updated_at": fields.DateTime
+
+})
+
+cart_schema=api.model('carts',{
+    "carts": fields.List(fields.Nested(carts_output_schema)),
+})
 
 
+vendor_input_schema = api.model('vendors',{
+    "id":fields.Integer,
+    "user_id" : fields.Integer, 
+    "fullnames" :fields.String,
+    "business_name" :fields.String,
+    "mobile_number" :fields.String,
+    "email_address" : fields.String,
+    "physical_address" : fields.String,
+    "map_location" : fields.String,
+    "product_list" : fields.String,
+    "image" : fields.String,
+    "created_at" : fields.DateTime,
+    "updated_at" : fields.DateTime, 
 
+})
+
+vendor_schema=api.model('vendor',{
+    "vendors": fields.List(fields.Nested(vendor_input_schema)) ,
+})
+
+
+product_input_schema = api.model('product', {
+    "id": fields.Integer,
+    "name": fields.String,
+    "description": fields.String,
+    "vendor_id": fields.Integer,
+    "category_id": fields.Integer,
+    "image": fields.String,
+    "price": fields.Integer,
+})
+product_schema = api.model("products", {
+    "products": fields.List(fields.Nested(product_input_schema))
+})
+
+order_input_schema = api.model('order', {
+    "id": fields.Integer,
+    "product_id": fields.Integer,
+    "cart_item_id": fields.Integer,
+    "cart_id": fields.Integer,
+    "user_id": fields.Integer,
+    "quantity": fields.Integer,
+    "status": fields.String,
+    "order_date": fields.DateTime,
+})
+order_schema = api.model('orders', {
+    "orders": fields.List(fields.Nested(order_input_schema))
+})
 
