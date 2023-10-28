83fe0b8b4b89c7a8fd41c96117374ea033802904
kipkirui63 <enocksang8356@gmail.com>
2023-10-28 09:15:56 +0300

    defined all the routes for HTTP verb POST operations on different resources

--- server/api/routes.py
+++ server/api/routes.py
@@ -166,177 +166,177 @@ class CartItemResource(Resource):
         return cart_item
 
 
-#             #*********POST**********
-# @ns.route('/users')
-# class Users(Resource):
-#     @ns.expect(user_input_schema)
-#     @ns.marshal_with(user_schema, code=201)
-#     def post(self):
-#         data = request.get_json()
-#         # Validation and processing logic
-#         new_user = User(
-#             username=data['username'],
-#             email=data['email'],
-#             public_id=str(uuid.uuid4())
-#         )
-#         new_user.set_password(data['password'])
-#         db.session.add(new_user)
-#         db.session.commit()
-#         return new_user, 201
+            #*********POST**********
+@ns.route('/users')
+class Users(Resource):
+    @ns.expect(user_input_schema)
+    @ns.marshal_with(user_schema, code=201)
+    def post(self):
+        data = request.get_json()
+        # Validation and processing logic
+        new_user = User(
+            username=data['username'],
+            email=data['email'],
+            public_id=str(uuid.uuid4())
+        )
+        new_user.set_password(data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user, 201
     
 
-# @ns.route('/vendors')
-# class Vendors(Resource):
-#     @ns.expect(vendor_input_schema)
-#     @ns.marshal_with(vendor_schema, code=201)
-#     def post(self):
-#         data = request.get_json()
-#         # Validation and processing logic
-#         new_vendor = Vendor(
-#             user_id=data['user_id'],
-#             fullnames=data['fullnames'],
-#             business_name=data['business_name'],
-#             mobile_number=data['mobile_number'],
-#             email_address=data['email_address'],
-#             physical_address=data['physical_address'],
-#             map_location=data['map_location'],
-#             product_list=data['product_list'],
-#             image=data['image']
-#         )
-#         db.session.add(new_vendor)
-#         db.session.commit()
-#         return new_vendor, 201
+@ns.route('/vendors')
+class Vendors(Resource):
+    @ns.expect(vendor_input_schema)
+    @ns.marshal_with(vendor_schema, code=201)
+    def post(self):
+        data = request.get_json()
+        # Validation and processing logic
+        new_vendor = Vendor(
+            user_id=data['user_id'],
+            fullnames=data['fullnames'],
+            business_name=data['business_name'],
+            mobile_number=data['mobile_number'],
+            email_address=data['email_address'],
+            physical_address=data['physical_address'],
+            map_location=data['map_location'],
+            product_list=data['product_list'],
+            image=data['image']
+        )
+        db.session.add(new_vendor)
+        db.session.commit()
+        return new_vendor, 201
 
 
-# @ns.route('/categories')
-# class Categories(Resource):
-#     @ns.expect(category_input_schema)
-#     @ns.marshal_with(category_schema, code=201)
-#     def post(self):
-#         data = request.get_json()
-#         # Validation and processing logic
-#         new_category = Category(
-#             name=data['name']
-#         )
-#         db.session.add(new_category)
-#         db.session.commit()
-#         return new_category, 201
+@ns.route('/categories')
+class Categories(Resource):
+    @ns.expect(category_input_schema)
+    @ns.marshal_with(category_schema, code=201)
+    def post(self):
+        data = request.get_json()
+        # Validation and processing logic
+        new_category = Category(
+            name=data['name']
+        )
+        db.session.add(new_category)
+        db.session.commit()
+        return new_category, 201
 
 
-# @ns.route('/carts')
-# class Carts(Resource):
-#     @ns.marshal_with(cart_schema, code=201)
-#     def post(self):
-#         new_cart = Cart()
-#         db.session.add(new_cart)
-#         db.session.commit()
-#         return new_cart, 201
+@ns.route('/carts')
+class Carts(Resource):
+    @ns.marshal_with(cart_schema, code=201)
+    def post(self):
+        new_cart = Cart()
+        db.session.add(new_cart)
+        db.session.commit()
+        return new_cart, 201
 
 
-# @ns.route('/cart_items')
-# class CartItems(Resource):
-#     @ns.expect(cart_item_input_schema)
-#     @ns.marshal_with(cart_item_schema, code=201)
-#     def post(self):
-#         data = request.get_json()
-#         photo_id = data.get('photo_id')
-#         quantity = data.get('quantity')
+@ns.route('/cart_items')
+class CartItems(Resource):
+    @ns.expect(cart_item_input_schema)
+    @ns.marshal_with(cart_item_schema, code=201)
+    def post(self):
+        data = request.get_json()
+        photo_id = data.get('photo_id')
+        quantity = data.get('quantity')
 
-#         # Validate that the photo with the given ID exists
-#         photo = photo.query.get(photo_id)
-#         if not photo:
-#             return {'message': 'Photo not found'}, 404
+        # Validate that the photo with the given ID exists
+        photo = photo.query.get(photo_id)
+        if not photo:
+            return {'message': 'Photo not found'}, 404
 
-#         # Create a new cart item
-#         new_cart_item = CartItem(
-#             photo_id=photo_id,
-#             quantity=quantity
-#         )
+        # Create a new cart item
+        new_cart_item = CartItem(
+            photo_id=photo_id,
+            quantity=quantity
+        )
 
-#         db.session.add(new_cart_item)
-#         db.session.commit()
+        db.session.add(new_cart_item)
+        db.session.commit()
 
-#         return new_cart_item, 201
-
-
-# @ns.route('/products')
-# class Products(Resource):
-#     @ns.expect(product_input_schema)
-#     @ns.marshal_with(product_schema, code=201)
-#     def post(self):
-#         data = request.get_json()
-#         name = data.get('name')
-#         description = data.get('description')
-#         price = data.get('price')
-#         vendor_id = data.get('vendor_id')
-#         category_id = data.get('category_id')
-#         image = data.get('image')
-
-#         # Validate that the vendor and category with the given IDs exist
-#         vendor = Vendor.query.get(vendor_id)
-#         category = Category.query.get(category_id)
-
-#         if not vendor:
-#             return {'message': 'Vendor not found'}, 404
-
-#         if not category:
-#             return {'message': 'Category not found'}, 404
-
-#         # Create a new product
-#         new_product = Product(
-#             name=name,
-#             description=description,
-#             price=price,
-#             vendor_id=vendor_id,
-#             category_id=category_id,
-#             image=image
-#         )
-
-#         db.session.add(new_product)
-#         db.session.commit()
-
-#         return new_product, 201
+        return new_cart_item, 201
 
 
-# @ns.route('/orders')
-# class Orders(Resource):
-#     @ns.expect(order_input_schema)
-#     @ns.marshal_with(order_schema, code=201)
-#     def post(self):
-#         data = request.get_json()
-#         product_id = data.get('product_id')
-#         cart_item_id = data.get('cart_item_id')
-#         user_id = data.get('user_id')
-#         quantity = data.get('quantity')
-#         status = data.get('status')
+@ns.route('/products')
+class Products(Resource):
+    @ns.expect(product_input_schema)
+    @ns.marshal_with(product_schema, code=201)
+    def post(self):
+        data = request.get_json()
+        name = data.get('name')
+        description = data.get('description')
+        price = data.get('price')
+        vendor_id = data.get('vendor_id')
+        category_id = data.get('category_id')
+        image = data.get('image')
 
-#         # Validate that the product, cart item, and user with the given IDs exist
-#         product = Product.query.get(product_id)
-#         cart_item = CartItem.query.get(cart_item_id)
-#         user = User.query.get(user_id)
+        # Validate that the vendor and category with the given IDs exist
+        vendor = Vendor.query.get(vendor_id)
+        category = Category.query.get(category_id)
 
-#         if not product:
-#             return {'message': 'Product not found'}, 404
+        if not vendor:
+            return {'message': 'Vendor not found'}, 404
 
-#         if not cart_item:
-#             return {'message': 'Cart Item not found'}, 404
+        if not category:
+            return {'message': 'Category not found'}, 404
 
-#         if not user:
-#             return {'message': 'User not found'}, 404
+        # Create a new product
+        new_product = Product(
+            name=name,
+            description=description,
+            price=price,
+            vendor_id=vendor_id,
+            category_id=category_id,
+            image=image
+        )
 
-#         # Create a new order
-#         new_order = Order(
-#             product_id=product_id,
-#             cart_item_id=cart_item_id,
-#             user_id=user_id,
-#             quantity=quantity,
-#             status=status
-#         )
+        db.session.add(new_product)
+        db.session.commit()
 
-#         db.session.add(new_order)
-#         db.session.commit()
+        return new_product, 201
 
-#         return new_order, 201
+
+@ns.route('/orders')
+class Orders(Resource):
+    @ns.expect(order_input_schema)
+    @ns.marshal_with(order_schema, code=201)
+    def post(self):
+        data = request.get_json()
+        product_id = data.get('product_id')
+        cart_item_id = data.get('cart_item_id')
+        user_id = data.get('user_id')
+        quantity = data.get('quantity')
+        status = data.get('status')
+
+        # Validate that the product, cart item, and user with the given IDs exist
+        product = Product.query.get(product_id)
+        cart_item = CartItem.query.get(cart_item_id)
+        user = User.query.get(user_id)
+
+        if not product:
+            return {'message': 'Product not found'}, 404
+
+        if not cart_item:
+            return {'message': 'Cart Item not found'}, 404
+
+        if not user:
+            return {'message': 'User not found'}, 404
+
+        # Create a new order
+        new_order = Order(
+            product_id=product_id,
+            cart_item_id=cart_item_id,
+            user_id=user_id,
+            quantity=quantity,
+            status=status
+        )
+
+        db.session.add(new_order)
+        db.session.commit()
+
+        return new_order, 201
 
 
 
