a1bf7bca96d11ad0660f2017df6623493b600d61
michellemwangi01 <michellemwangim@gmail.com>
2023-11-03 11:38:57 +0300

    created tests

---
+++ server/tests.py
@@ -0,0 +1,16 @@
+import pytest
+from api import  app, db, User  # Import your User model and SQLAlchemy db instance
+from flask import json
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+def test_get_users(client):
+    response = client.get('users/users')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
